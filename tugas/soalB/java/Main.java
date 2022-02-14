5ea04e63e9a93a8f99357ecb9cf5e50aa40125f4
Amim16 <91143712+Amim16@users.noreply.github.com>
2022-02-15 01:31:12 +0700

    Add files via upload

--- tugas/soalB/java/Main.java
+++ tugas/soalB/java/Main.java
@@ -15,17 +15,62 @@ public class Main {
         /* instansiasi dengan constructor */
         TimBola tim2 = new TimBola("Barka", "Bekasi", 2002, "Ucup");
 
+        /* meminta inputan user */
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        try{
+            n = sc.nextInt();
+        }catch(Exception e){}
+
+        // alokasi objek sebanyak n dalam bentuk array
+        TimBola[] arrTim = new TimBola[n];
+
+        int i, since = 0; 
+        String name = "x", country = "-", player = "-";
+
+        // input data sebanyak n
+        for(i=0;i<n;i++){
+            try{
+                name = sc.next();
+            }catch(Exception e){}
+
+            try{
+                country = sc.next();
+            }catch(Exception e){}
+
+            try{
+                since = sc.nextInt();
+            }catch(Exception e){}
+
+            try{
+                player = sc.next();
+            }catch(Exception e){}
+
+            // instansiasi tiap objek
+            arrTim[i] = new TimBola(name, country, since, player);
+        }
+
         /* tampilkan output */
-        System.out.println("--- Tim 1 ---");
+        System.out.println("\n--- Tim 1 ---");
         System.out.println("Nama Tim : " + tim1.getNama());
         System.out.println("Asal Negara : " + tim1.getAsalNegara());
         System.out.println("Tahun Berdiri : " + tim1.getSejak());
         System.out.println("Pemain : " + tim1.getPemain());
 
-        System.out.println("--- Tim 2 ---");
+        System.out.println("\n--- Tim 2 ---");
         System.out.println("Nama Tim : " + tim2.getNama());
         System.out.println("Asal Negara : " + tim2.getAsalNegara());
         System.out.println("Tahun Berdiri : " + tim2.getSejak());
         System.out.println("Pemain : " + tim2.getPemain());
+
+        System.out.println("\n--{ Output data inputan user }--");
+
+        for(i=0;i<n;i++){
+            System.out.println("\n--- Tim " + (i+1) + " Inputan User ---");
+            System.out.println("Nama Tim : " + arrTim[i].getNama());
+            System.out.println("Asal Negara : " + arrTim[i].getAsalNegara());
+            System.out.println("Tahun Berdiri : " + arrTim[i].getSejak());
+            System.out.println("Pemain : " + arrTim[i].getPemain());
+        }
     }
 }
